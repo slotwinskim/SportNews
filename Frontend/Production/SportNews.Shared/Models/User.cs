a0ed79c587aee9adf1921e998f4d2b63c0b5d83d
Marcin Słotwiński (MNSL) <mnsl@dgs.com>
2019-03-06 11:39:24 +0100

    Cleared all, added .gitignore, created projects

--- Frontend/Production/SportNews.Shared/Models/User.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SportNews.Shared.Models
-{
-	public class User : Entity
-	{
-		public string Name { get; set; }
-	}
-}
---
+++ src/SportNews.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SportNews.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
